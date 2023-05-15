8b367c497cbe25b6258807f409d7b37ddfb53e9a
Nrosy10 <117885685+Nrosy10@users.noreply.github.com>
2023-05-15 10:34:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // complete the given function
 
 function palindrome(str){
-
+       var str2= str.lowercase();
+	var i=0;
+	var j=str2.length-1;
+	while(i<j)
+	{
+		if(str2.charAt(i)===str2.charAt(j))
+		{
+			i++;
+		    j--;
+		}
+		else
+		{
+			return false;
+		}
+		
+	}
+	return true;
 }
+
 module.exports = palindrome
